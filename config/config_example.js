cefef1d7f1478c063780750dc4d95df022918be5
Sabrina Lua <sabrina.lua@b2be.com>
2017-10-13 17:04:21 +0800

    commit2

---
+++ config/config_example.js
@@ -0,0 +1,1 @@
+//name the file config.js
---
+++ config/params_example.js
@@ -0,0 +1,13 @@
+//name the file params.js
+
+//example definition
+/*
+function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value:      value,
+    });
+}
+
+define("PI", "pie");
+define("dbUrl", "mongodb://localhost:27017/anonymo");
+*/
